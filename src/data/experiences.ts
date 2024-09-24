e278591907ff360031c5272170cf6de67d6c225b
Daniel Banariba <banaribad@gmail.com>
2024-09-24 14:01:35 -0600

    Componenete para la experiencia laboral

---
+++ src/data/experiences.ts
@@ -0,0 +1,28 @@
+// TODO poner las experiencias reales de los trabajos
+
+export const experiences = [
+  {
+    title: "React.js Developer",
+    companyName: "Chaldal",
+    icon: "/icon/empresas/claro.svg",
+    iconBg: "#383E56",
+    date: "March 2020 - April 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    companyName: "10 Minute School",
+    icon: "/icon/empresas/tigo.svg",
+    iconBg: "#E6DEDD",
+    date: "April 2021 - Sep 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    ],
+  },
+];
