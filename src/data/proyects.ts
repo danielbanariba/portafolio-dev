5f769cf31ce62068292c550b462eaf6c26671ade
Daniel Banariba <banaribad@gmail.com>
2024-09-25 04:18:45 -0600

    Proyectos Personales de programacion

--- src/data/proyects.ts
+++ src/data/proyects.ts
@@ -1,19 +1,49 @@
 // TODO: Actualizar con los proyectos reales que vayas terminando
 export const projects = [
     {
-      title: "Proyecto 1",
-      imageUrl: "/ruta-a-tu-imagen-1.jpg",
-      description: "Descripción breve del proyecto 1.",
-      technologies: ['React', 'Node.js', 'MongoDB'],
-      githubUrl: "https://github.com/tuusuario/proyecto1",
-      projectUrl: "https://proyecto1.com"
+      title: "Analyzepy",
+      imageUrl: "project/analizador_lexico.png",
+      description: "Es una herramienta que traduce código de Python a JavaScript, utilizando técnicas avanzadas de parsing y generación de código",
+      technologies: ['Python', 'Reflex'],
+      githubUrl: "https://github.com/danielbanariba/analizador-lexico",
+      projectUrl: "https://analyzepy.reflex.run/"
     },
     {
-      title: "Proyecto 2",
-      imageUrl: "/ruta-a-tu-imagen-2.jpg",
-      description: "Descripción breve del proyecto 2.",
-      technologies: ['Vue.js', 'Express', 'PostgreSQL'],
-      githubUrl: "https://github.com/tuusuario/proyecto2",
-      projectUrl: "https://proyecto2.com"
+      title: "Resuelve Sistemas de Ecuaciones",
+      imageUrl: "project/sistemas_de_ecuaciones.png",
+      description: "Aplicación web para resolver sistemas de ecuaciones lineales, aplicando métodos algebraicos y ofreciendo una interfaz amigable para los usuarios.",
+      technologies: ['Python', 'Reflex'],
+      githubUrl: "https://github.com/danielbanariba/sistemas-de-ecuaciones",
+      projectUrl: "https://resuelvesistema.reflex.run/"
+    },
+    {
+      title: "Canal de YouTube Automatizado",
+      imageUrl: "project/click-auto-editor.png",
+      description: "Es un programa que automatiza la edición de videos para un mi canal de YouTube, aplicando efectos, transiciones y subiendo el video a la plataforma de forma automatica.",
+      technologies: ['Python', 'React'],
+      githubUrl: "https://github.com/danielbanariba/click-auto-editor",
+      // TODO Modificar el componenete para que cuando no reciba la url del proyecto, no muestre el botón
+    },
+    {
+      title: "Web Scraping",
+      imageUrl: "project/web-scraping.jpg",
+      description: "Para que que mi canal de YouTube tenga contenido contantemente, he creado un programa que se encarga de buscar musica libre de derechos de autor en internet y descargarlas.",
+      technologies: ['Python'],
+      githubUrl: "https://github.com/danielbanariba/scrapper-deathgrind",
+    },
+    {
+      title: "Arsenal de Odio Website",
+      imageUrl: "project/arsenal-de-odio.png",
+      description: "Arsenal De Odio es una banda de Thrash Metal de Honduras, quise plasmar su espíritu como banda en la pagina web. Aquí se puede encontrar todo relacionado con la banda, sus integrantes, discografía, mercadería, etc.",
+      technologies: ['Python', 'Reflex', 'Javascript'],
+      githubUrl: "https://github.com/danielbanariba/arsenal-de-odio-web",
+      projectUrl: "https://arsenaldeodio.reflex.run/"
+    },
+    {
+      title: "SoundCloud Clone",
+      imageUrl: "project/soundcloud.jpg",
+      description: "Analizar y clonar la funcionalidad de SoundCloud, creacion de la base de datos, backend y frontend.",
+      technologies: ['OracleSQL', 'Python', 'FastAPI', 'HTML', 'CSS'],
+      githubUrl: "https://github.com/danielbanariba/soundclound",
     },
   ];
