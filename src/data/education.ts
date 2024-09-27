3ca791998ebb71da1e4f5b35650ec7dabcbaf6d5
Daniel Banariba <banaribad@gmail.com>
2024-09-26 20:41:03 -0600

    Refactorice las skills x3, y puse formacion profesional

---
+++ src/data/education.ts
@@ -0,0 +1,27 @@
+export interface Education {
+    title: string;
+    institution: string;
+    date: string;
+    logoUrl: string;
+  }
+  
+  export const educations: Education[] = [
+    {
+      title: "Ingeniería en Sistemas",
+      institution: "Universidad Nacional Autónoma de Honduras",
+      date: "2019 - 2024",
+      logoUrl: "icon/education/unah.svg"
+    },
+    {
+      title: "Certificación en Desarrollo Web Full Stack",
+      institution: "Platzi",
+      date: "2022 - 2023",
+      logoUrl: "icon/education/platzi.svg"
+    },
+    {
+        title: "Certificación en Desarrollo Web Full Stack",
+        institution: "Platzi",
+        date: "2022 - 2023",
+        logoUrl: "icon/education/platzi.svg"
+      },
+  ];
---
+++ src/data/skills.ts
@@ -0,0 +1,24 @@
+export interface Skill {
+    name: string;
+    icon: string;
+    category: 'Backend' | 'Frontend' | 'Frameworks' | 'Gestores de Bases de Datos';
+  }
+  
+  export const skills: Skill[] = [
+    { name: 'Python', icon: '/icon/python.svg', category: 'Backend' },
+    { name: 'Java', icon: '/icon/java.svg', category: 'Backend' },
+    { name: 'C++', icon: '/icon/cpp.svg', category: 'Backend' },
+    { name: 'Typescript', icon: '/icon/typescript.svg', category: 'Backend' },
+    { name: 'HTML', icon: '/icon/html5.svg', category: 'Frontend' },
+    { name: 'CSS', icon: '/icon/css.svg', category: 'Frontend' },
+    { name: 'JavaScript', icon: '/icon/javascript.svg', category: 'Frontend' },
+    { name: 'Tailwind CSS', icon: '/icon/tailwindcss.svg', category: 'Frontend' },
+    { name: 'Astro', icon: '/icon/astro.svg', category: 'Frameworks' },
+    { name: 'Reflex', icon: '/icon/reflex.svg', category: 'Frameworks' },
+    { name: 'FastAPI', icon: '/icon/fastapi.svg', category: 'Frameworks' },
+    { name: 'Spring', icon: '/icon/spring.svg', category: 'Frameworks' },
+    { name: 'Oracle', icon: '/icon/oracle.svg', category: 'Gestores de Bases de Datos' },
+    { name: 'MySQL', icon: '/icon/mysql.svg', category: 'Gestores de Bases de Datos' },
+    { name: 'PostgreSQL', icon: '/icon/postgresql.svg', category: 'Gestores de Bases de Datos' },
+    { name: 'MongoDB', icon: '/icon/mongodb.svg', category: 'Gestores de Bases de Datos' },
+  ];
