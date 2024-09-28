c7d07a2f042a01be0ff39206728e344b70de6167
Daniel Banariba <banaribad@gmail.com>
2024-09-27 20:38:10 -0600

    bug en el navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import gsap from "gsap";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()]
