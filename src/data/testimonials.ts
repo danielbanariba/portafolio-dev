4134360bff42d1c012f12bfe3e27368aadc0408f
Daniel Banariba <banaribad@gmail.com>
2024-09-27 23:25:17 -0600

    Testimonials Components

---
+++ src/data/testimonials.ts
@@ -0,0 +1,29 @@
+// testimonials.ts
+
+export const testimonials = [
+  {
+    name: "Ing. Alex Moncada",
+    role: "PS Core Engineer Planning & Docente Universitario",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nunc nec nisl eleifend ultricies. Nullam nec nunc nec nisl eleifend ultricies. Nullam nec nunc nec nisl eleifend ultricies.",
+    linkedInUrl: "https://www.linkedin.com/in/ahmg1989/",
+    iconBg: "#4B5563",
+  },
+  {
+    name: "Ing. Uayeb Caballero",
+    role: "Data Scientist | Data Engineer | React Developer | EdTech",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nunc nec nisl eleifend ultricies. Nullam nec nunc nec nisl eleifend ultricies. Nullam nec nunc nec nisl eleifend ultricies.",
+    linkedInUrl: "https://www.linkedin.com/in/uayebcaballero/",
+    iconBg: "#1E40AF",
+  },
+  // TODO: Agregar más testimonios
+  // Estoy indeciso si poner a Oscar Cruz = https://www.linkedin.com/in/oscar-cruz-26163875/
+  // Andrea Matinez = https://www.linkedin.com/in/andrealmartinez/
+  // Nestor Lopez Luque = https://www.linkedin.com/in/n%C3%A9stor-l%C3%B3pez-luque-7b436b86/
+  // {
+  //   name: "Ing. Carlos Rodríguez",
+  //   role: "Instructor de Programación Avanzada",
+  //   comment: "La habilidad de Daniel para aprender nuevas tecnologías rápidamente es impresionante. Su código es limpio, bien documentado y siempre sigue las mejores prácticas de la industria.",
+  //   linkedInUrl: "https://www.linkedin.com/in/carlosrodriguez/",
+  //   iconBg: "#3730A3",
+  // },
+];
