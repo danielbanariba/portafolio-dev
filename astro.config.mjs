687f1104ef716df7738cf5016dd9fca1e9e6957b
Daniel Banariba <banaribad@gmail.com>
2024-09-30 00:57:06 -0600

    Cambio de los comentarios a la sintaxis de astro x2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
-import gsap from "gsap";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()]
